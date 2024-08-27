734389f4e1dc36a4422a00645c1e9a62a4d4f5bd
jurakhon <jurakhon@gmail.com>
2024-08-27 16:28:47 +0500

    task2

--- hometask.py
+++ hometask.py
@@ -4,3 +4,17 @@
 #     print(a.count("potato"))
 #
 # potatoes(input())
+
+
+# Task2:
+# def sum_of_elements(a:list):
+#
+#     sum = 0
+#     for i in a:
+#         if i>5:
+#             sum+=i
+#     print(sum)
+#
+# my_list = input()
+# my_list = [int(x) for x in my_list.split()]
+# sum_of_elements(my_list)
