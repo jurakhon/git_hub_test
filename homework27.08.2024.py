1e045b135444a9589f379ae64f77e9080a39685c
jurakhon <jurakhon@gmail.com>
2024-08-28 15:28:40 +0500

    task1

---
+++ homework27.08.2024.py
@@ -0,0 +1,14 @@
+# task1:
+# from datetime import datetime, date, time, timedelta
+#
+# def halloween(a):
+#
+#     res = datetime.strptime(a, "%Y/%m/%d")
+#     month = res.month
+#     day = res.day
+#     if month==10 and day==31:
+#         print("Bonfire toffee")
+#     else:
+#         print("toffee")
+#
+# halloween(input("Enter date in the following format:--->2013/10/31"))
