d03b82b9c8f6874a3e8d6ad019ae661d132a4086
jurakhon <jurakhon@gmail.com>
2024-08-28 15:33:46 +0500

    task4

--- homework27.08.2024.py
+++ homework27.08.2024.py
@@ -14,11 +14,22 @@
 # halloween(input("Enter date in the following format:--->2013/10/31"))
 
 
-Task3:
-from datetime import datetime, date, time, timedelta
+# Task3:
+# from datetime import datetime, date, time, timedelta
+#
+# def after_months(a,b:int)
+#
+#     new_year = datetime.strptime(a,"%Y")
+#     res = b//12
+#     print(res+new_year.year)
 
-def after_months(a,b:int)
+# Task4:
+# from datetime import datetime
+#
+#
+# def converter(a):
+#     res = datetime.strptime(a,"%d/%M/%Y")
+#     new = datetime.strftime(res,"%Y%M%d")
+#
+#     print(new)
 
-    new_year = datetime.strptime(a,"%Y")
-    res = b//12
-    print(res+new_year.year)
