b9af002b2cce5add0cf5e9a254be0d3b23db4803
jurakhon <jurakhon@gmail.com>
2024-08-29 16:34:20 +0500

    task3

---
+++ homework28.08.2024.py
@@ -0,0 +1,9 @@
+# Task3:
+# import random
+#
+# my_list = ["!","%","#","&","@","^","(","*",")",1,2,3,4,5,6,7,8,9,0,"h","v","x","b","o","l","g","r","u","i","m","Q","W","R","T","Y","A","S","D","F","G","N","Z"]
+#
+# n = int(input("enter your desired password length:--->"))
+# my_pass = random.choices(my_list,k=n)
+#
+# print(*my_pass,sep="")
