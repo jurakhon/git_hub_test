fd6624101b7854b99c9e818954fa3ca521fcdc38
jurakhon <jurakhon@gmail.com>
2024-09-04 14:39:39 +0500

    task1

---
+++ homework03.09.2024.py
@@ -0,0 +1,12 @@
+# Task1:
+# def recursive_sum(a):
+#     if len(a) == 0:
+#         return 0
+#     else:
+#         return a[0] + recursive_sum(a[1:])
+#
+# my_list = [1, 2, 3, 5, 7, 3, 2, 9]
+#
+# res = recursive_sum(my_list)
+#
+# print(f"The sum of {my_list} is: {res}")
