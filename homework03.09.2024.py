5f3975ce85266c68a878f2b571d4e5775c303900
jurakhon <jurakhon@gmail.com>
2024-09-04 15:16:42 +0500

    task 5

--- homework03.09.2024.py
+++ homework03.09.2024.py
@@ -37,3 +37,12 @@
 #     if n <= 0:
 #         return 0
 #     return n + sum_series(n - 2)
+
+
+# Task5:
+# def power(a, b):
+#
+#     if b == 0:
+#         return 1
+#     else:
+#         return a * power(a, b - 1)
