a1c1c42270bc4cf0f078847757201a0b55a6b3e3
jurakhon <jurakhon@gmail.com>
2024-10-17 13:42:23 +0500

    homework16.10.2024

---
+++ homework16.10.2024.py
@@ -0,0 +1,39 @@
+#task2:
+# def upper_text(func):
+#     def wrapper():
+#         print("before calling function")
+#         func()
+#         print("after calling function")
+#     return wrapper
+#
+# @upper_text
+# def say_goodbye():
+#     print("Good Bye!!!")
+#
+# say_goodbye()
+
+# task1:
+# def upper_text(func):
+#     def wrapper():
+#         print("test")
+#         func()
+#     return wrapper
+#
+# @upper_text
+# def say_hi():
+#     print("hi everyone!!!")
+#
+# say_hi()
+
+# task3:
+# from enum import Enum
+#
+# class Day(Enum):
+#     MONDAY = 1
+#     TUESDAY = 2
+#     WEDNESDAY = 3
+#     THURSDAY = 4
+#     FRIDAY = 5
+#     SATURDAY = 6
+#     SUNDAY = 7
+#
